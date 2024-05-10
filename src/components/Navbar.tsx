114b8afa3c411abb02967aeeb691b95fa37fe18c
Danyal Imran <dan.imran97@gmail.com>
2024-05-09 23:44:22 -0400

    working on searchbar component and added react-icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaMoon } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import { MdMyLocation } from "react-icons/md";
 
 type Props = {}
 
@@ -8,8 +11,18 @@ export default function Navbar({}: Props) {
         <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
             <p className="flex items-center justify-center gap-2">
                 <h2 className="text-gray-500 text-3xl">Weather App</h2>
+                <FaMoon className="text-3xl mt-1 text-blue-400"/>
             </p>
-        Navbar
+
+            <section className="flex gap-2 items-center">
+                <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"/>
+                <IoLocationSharp className="text-3xl text-gray-900 hover:opacity-80 cursor-pointer" />
+                <p className="text-slate-900/80 text-sm"> Hamilton </p>
+
+                <div>
+                    {/* SearchBox */}
+                </div>
+            </section>
         </div>
     </nav>
   )
