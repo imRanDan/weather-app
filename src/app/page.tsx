4dbff3ecf39f58128eae5841a6121a6661639160
Danyal Imran <dan.imran97@gmail.com>
2024-05-16 08:08:38 -0400

    adding in weatherdetail and implemented react-wuery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,77 @@
+"use client"
+
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import { useQuery } from "react-query";
 
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_jf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_text: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+  };
+}
+
+
+// https://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=de1e9c9e3c3ed5f7e4f954f578362542&cnt=56
 export default function Home() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json(),
+      ),
+  });
+
+  if (isPending) return 'Loading...'
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
